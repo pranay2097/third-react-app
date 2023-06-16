dfddc1ed362ef662597d742b533b8440db5b6378
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-17 04:12:49 +0530

    bootstratpmultiple function  setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+//uder defined tag: <App/>
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      {/* <h1>Hello world</h1> */}
+      <AppHeader />
+      <AppBody/>
+      <AppFooter/>
+    </>
+  );
+}
+
+//user defined tag: <AppHeader/>
+function AppHeader() {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+}
+
+//user defined tag: <AppBody/>
+function AppBody() {
+  return (
+    <>
+      <h1>App body</h1>
+    </>
+  );
+}
+
+//user defined tag: <AppFooter/>
+function AppFooter() {
+  return (
+    <>
+      <h1>App Footer</h1>
     </>
   );
 }
