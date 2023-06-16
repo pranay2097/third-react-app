b130dcce4b1de44d87201d25154ba36d8dad39b0
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-17 05:04:21 +0530

    function structure

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ function App() {
   );
 }
 
+//UserDefinedTag
 function CounterApp() {
+  //Data Member ::Optional
+  let counter=1;
+  let id = 100;
+  let pi = 3.14;
+  let str = "hello world";
+  let active= true;
+
+  //Member Function ::Optional
+  let LikeMeAction = () => {};
+  let dislikeMeAction = () => {};
+  
   return (
     <>
-      <h1>1</h1>
+      <h1>{counter}</h1>
       <input type="button" value="Like Me!" />
     </>
   );
