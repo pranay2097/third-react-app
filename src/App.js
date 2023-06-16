668276278d7c7c7de7a4e130067667cb86474f5a
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-17 05:11:43 +0530

    button click

--- src/App.js
+++ src/App.js
@@ -9,21 +9,16 @@ function App() {
 
 //UserDefinedTag
 function CounterApp() {
-  //Data Member ::Optional
-  let counter=1;
-  let id = 100;
-  let pi = 3.14;
-  let str = "hello world";
-  let active= true;
-
-  //Member Function ::Optional
-  let LikeMeAction = () => {};
-  let dislikeMeAction = () => {};
+  //Member function
+  let LikeMeAction = () => {
+    // alert();
+    console.log("i am button click");
+  };
   
+  //JSX :: view :: user-interface :: required
   return (
     <>
-      <h1>{counter}</h1>
-      <input type="button" value="Like Me!" />
+      <input type="button" value="Like Me!" onClick={LikeMeAction}/>
     </>
   );
 }
