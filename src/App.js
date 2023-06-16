ec0a5a10d168e9f14b75fdbb6b93d337bc958d50
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-17 04:31:18 +0530

    footer is done

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function AppHeader() {
 function AppBody() {
   return (
     <>
-      <div className="alert alert-success fs-3">
+      <div className="alert alert-success fs-3 mb-0">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro omnis tempora est? Sit quaerat accusantium debitis laboriosam illum porro ipsa magnam? Nesciunt itaque inventore labore veritatis pariatur, reprehenderit praesentium quod? Iure pariatur recusandae perferendis aliquid, eaque quaerat aspernatur magni nesciunt vel eius voluptate impedit ipsa iste voluptatibus odio quibusdam saepe cum laudantium sunt. Maiores quasi architecto molestias beatae modi ducimus culpa optio aut, adipisci consectetur. Alias consectetur deleniti inventore ducimus harum quasi ab esse facilis voluptatum ipsam ipsa earum culpa soluta, adipisci doloremque minima laudantium eos! Explicabo vitae illo voluptate doloribus iusto, laudantium, rerum, et culpa omnis animi blanditiis saepe!
       </div>
     </>
@@ -36,7 +36,10 @@ function AppBody() {
 function AppFooter() {
   return (
     <>
-      <h1>App Footer</h1>
+      <div className="bg-secondary text-light d-flex justify-content-center align-items-center"
+      style={{height:"200px"}}>
+        Lorem ipsum dolor sit amet.
+      </div>
     </>
   );
 }
