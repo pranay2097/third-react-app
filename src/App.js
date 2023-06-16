5e52940f2f96b216484a2c6edeea324972e394db
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-17 04:20:30 +0530

    app body

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function AppHeader() {
 function AppBody() {
   return (
     <>
-      <h1>App body</h1>
+      <div className="alert alert-success fs-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro omnis tempora est? Sit quaerat accusantium debitis laboriosam illum porro ipsa magnam? Nesciunt itaque inventore labore veritatis pariatur, reprehenderit praesentium quod? Iure pariatur recusandae perferendis aliquid, eaque quaerat aspernatur magni nesciunt vel eius voluptate impedit ipsa iste voluptatibus odio quibusdam saepe cum laudantium sunt. Maiores quasi architecto molestias beatae modi ducimus culpa optio aut, adipisci consectetur. Alias consectetur deleniti inventore ducimus harum quasi ab esse facilis voluptatum ipsam ipsa earum culpa soluta, adipisci doloremque minima laudantium eos! Explicabo vitae illo voluptate doloribus iusto, laudantium, rerum, et culpa omnis animi blanditiis saepe!
+      </div>
     </>
   );
 }
